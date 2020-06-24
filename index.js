5f84d2d71e48686458c6b4a89183b147a809c423
Duy <10cb4tranbaoduy@gmail.com>
2020-06-24 21:21:00 +0700

    Change console.log

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ var Cat = require('./Cat');
 var dog = new Dog('Jack', 2);
 var cat = new Cat('Tom', 1);
 dog.sayHi();
-console.log("Dog eat cat");
 dog.eat(cat);
-console.log('Inside stomach of Dod: ', dog.stomach);
+console.log('This is my stomach: ', dog.stomach);
